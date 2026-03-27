88cbc2c423e3853da5d027b523cb6e8bc782f641
chyunj2-adfs <chyunj2@gmail.com>
2026-03-27 10:14:23 +0900

    Update 1-3.c

--- code/1-3/1-3.c
+++ code/1-3/1-3.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include "util.h"
 extern void task2();
-extern void logger_global(const char* message);
+extern void logger_global(char* message);
 void task1()
 {
     for(int i = 0;i < 5;i++){
