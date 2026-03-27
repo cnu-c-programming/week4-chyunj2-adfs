b0ea6b75d087efda07ca75fade0073c8c8314c42
chyunj2-adfs <chyunj2@gmail.com>
2026-03-27 09:53:08 +0900

    Update 1-3-2.c

---
+++ code/1-3/1-3-2.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "util.h"
+void logger_global(const char* message) {
+    static int counter = 0;
+    printf("log:%2d: %s\n", counter++, message);
+}
+void task2()
+{
+    for(int i = 0;i < 100;i++){
+        logger_static_inline("task2");
+        logger_global("task2");
+    }
+    return 0;
+}
